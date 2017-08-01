cd43df13fab92592bf6dd43fdb890500ef156c54
JERRY <jerrintkg@gmail.com>
2017-08-01 15:27:27 +0530

    demo

---
+++ application/views/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Particle Network Demo</title>
+  
+  
+  
+      <link rel="stylesheet" href="assets/css/style.css">
+
+  
+</head>
+
+<body>
+  <h1>CHETTAYEES</h1>
+<canvas></canvas>
+  
+    <script src="assets/js/index.js"></script>
+
+</body>
+</html>
